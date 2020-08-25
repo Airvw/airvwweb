d09c671968151e04fe20c7451dc7db0e7efe1164
DoubleK <airvw14@gmail.com>
2020-08-25 17:08:29 +0900

    수정 테스트 코드 + 전체 흐름 파악 (h2-console에 들어가지지 않는다. 이유는 아직 모름)

--- src/main/java/com/airvwweb/springboot/service/posts/PostsService.java
+++ src/main/java/com/airvwweb/springboot/service/posts/PostsService.java
@@ -20,6 +20,11 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+//    update 기능에서는 쿼리를 날리는 부분이 없다.(내 생각에는 PostsRepository의 CRUD 메소드가 활용이 안됬다는 뜻 인것 같다.)
+//    JPA의 영속성 컨텍스트 때문-> 엔티티를 영구 저장하는 환경. jPA의 핵심내용은 엔티티가 영속성 컨텍스트에 포함되어 있냐 아니냐로 갈림
+//    JPA의 엔티티 매니저가 활성화된 상태로 트랜잭션 안에서 데이터베이스에서 데이터를 가져오면 이 데이터는 영속성 컨텍스트가 유지된 상태
+//    -> 이 상태에서 해당 데이터 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영함. 그러므로 update 쿼리가 필요없음.
+//    이 개념은 더티 체킹
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id)
