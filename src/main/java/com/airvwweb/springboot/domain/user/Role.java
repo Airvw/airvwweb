9f954d7ab95911bf728b76aa88bea5171687e83d
DoubleK <airvw14@gmail.com>
2020-08-31 00:00:19 +0900

    구글 로그인 연동 코드 해석 필요

--- src/main/java/com/airvwweb/springboot/domain/user/Role.java
+++ src/main/java/com/airvwweb/springboot/domain/user/Role.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 //각 사용자의 권한을 관리할 Enum 클래스
+//한정된 값만을 가지는 데이터 타입이 열거 타입(Enumeration type)이다
 public enum Role {
 //    스프링 서큐리티에서 권한 코드에 항상 ROLE_이 앞에 있어야만 한다.
     GUEST("ROLE_GUEST", "손님"),
--- src/main/java/com/airvwweb/springboot/domain/user/UserRepository.java
+++ src/main/java/com/airvwweb/springboot/domain/user/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// User의 CRUD
 public interface UserRepository extends JpaRepository<User, Long> {
 //    findByEmail : 소셜 로그인으로 반환되는 값 중 email을 통해 기존 회원인지 판단하기 위한 메소드
     Optional<User> findByEmail(String email);
